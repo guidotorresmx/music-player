702bd70ac6dcb9e5a7bc68cb16017e7a05295d7d
Marco Guido <guido-torres@hotmail.com>
2021-12-19 17:22:07 -0500

    js: added script level variables

--- src/js/script.js
+++ src/js/script.js
@@ -11,3 +11,13 @@ const playNext = $(`#skip-next`)
 // |---progress
 const currentTime = $(`#progress-time`)
 const totalTime = $(`#total-time`)
+
+
+// Script level variables
+const playlist = [
+    "https://www.fesliyanstudios.com/musicfiles/2015-09-25_-_Old_Video_Game_Music_1_-_David_Fesliyan.mp3",
+    "https://www.fesliyanstudios.com/musicfiles/2019-01-10_-_Land_of_8_Bits_-_Stephen_Bennett_-_FesliyanStudios.com.mp3",
+]
+
+playingIndex = 0
+const song = new Audio()
