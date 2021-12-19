93d7178701aa06878288be92c5d8591d782f1997
Marco Guido <guido-torres@hotmail.com>
2021-12-19 18:20:41 -0500

    js: added Audio basic funcionality

--- src/js/script.js
+++ src/js/script.js
@@ -31,3 +31,33 @@ window.addEventListener(`load`, () => {
                 `<li class="playlist-element" data-index="${index}">${item.innerHTML}</li>`
         })
 })
+
+play.addEventListener(`clic`, function(event) {
+    song.play()
+    pause.classList.remove(`hidden`)
+    play.classList.add(`hidden`)
+})
+
+pause.addEventListener(`clic`, function(event) {
+    song.play()
+    play.classList.remove(`hidden`)
+    pause.classList.add(`hidden`)
+
+})
+
+playNext.addEventListener(`clic`, function(event) {
+    const nextIndex = (playingIndex + 1) > playlist.length - 1 ? 0 : playingIndex + 1;
+    loadSongFromPlaylistbyId(nextIndex);
+})
+
+playPrev.addEventListener(`clic`, function(event) {
+    const nextIndex = (playingIndex - 1) < 0 ? playlist.length - 1 : playingIndex - 1;
+    loadSongFromPlaylistById(nextIndex);
+})
+
+const loadSongFromPlaylistById = (id = 0) => {
+    playingIndex = id
+    song.src = playlist[playingIndex]
+    if (!song.paused)
+        song.play()
+}
