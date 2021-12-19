fe30b2dbb3b4d4cc3438ab96ccd2f94b3e68335c
Marco Guido <guido-torres@hotmail.com>
2021-12-19 16:40:08 -0500

    js: added utils script

--- src/js/utils.js
+++ src/js/utils.js
@@ -2,3 +2,7 @@
 
 export $ = selector = document.querySelector(selector);
 export $$ = selector = document.querySelectorAll(selector);
+
+export const formatTime = (time) => {
+        return `${Math.floor(time / 60)}:${Math.floor(time % 60).toString().padStart(2, `0`)}`
+  }
