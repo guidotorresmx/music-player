1231a618364d92ff457e767d84eb58e171d3cb77
Marco Guido <guido-torres@hotmail.com>
2021-12-19 18:30:12 -0500

    js: #fixed utilities typos

--- src/js/utils.js
+++ src/js/utils.js
@@ -1,7 +1,7 @@
 //Utility functions for the application
 
-export $ = selector = document.querySelector(selector);
-export $$ = selector = document.querySelectorAll(selector);
+export const $ = (selector) => document.querySelector(selector);
+export const $$ = (selector) => document.querySelectorAll(selector);
 
 export const formatTime = (time) => {
         return `${Math.floor(time / 60)}:${Math.floor(time % 60).toString().padStart(2, `0`)}`
