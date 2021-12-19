6e308cbc5d3aab5cc112225577f96df12bbf5b3b
Marco Guido <guido-torres@hotmail.com>
2021-12-19 17:16:52 -0500

    js: added document elements selectors

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,13 @@
-import { $, $$ } from './utils.js'
-import { playlist } from '../assets/music.js'
+import { formatTime } from './utils.js'
+
+// Documern elements
+// |---controls
+const audioPlayer = $(`.controls`)
+const play = $(`#play`)
+const pause = $(`#pause`)
+const playPrev = $(`#skip-prev`)
+const playNext = $(`#skip-next`)
+
+// |---progress
+const currentTime = $(`#progress-time`)
+const totalTime = $(`#total-time`)
